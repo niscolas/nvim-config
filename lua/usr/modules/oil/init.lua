e03c465fb2173a43f563484c8ddf35d3b839c8aa
niscolas <nic.cp@hotmail.com>
2023-01-24 15:43:56 -0300

    feat: better oil config

--- lua/usr/modules/oil/init.lua
+++ lua/usr/modules/oil/init.lua
@@ -1,2 +1,41 @@
-require("oil").setup()
+require("oil").setup {
+    columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        "mtime",
+    },
+    keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-s>"] = "actions.select_split",
+        ["<C-j>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-l>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["g."] = "actions.toggle_hidden",
+        ["o"] = {
+            callback = function()
+                local oil = require("oil")
+                local file_path = oil.get_current_dir()
+                    .. "/"
+                    .. oil.get_cursor_entry().name
+
+                if vim.fn.executable("mimeo") then
+                    vim.fn.system { "mimeo", file_path }
+                elseif vim.fn.executable("xdg-open") then
+                    vim.fn.system { "xdg-open", file_path }
+                end
+            end,
+        },
+    },
+    use_default_keymaps = false,
+    view_options = {
+        show_hidden = true,
+    },
+}
 require("usr.modules.oil.keymap")
