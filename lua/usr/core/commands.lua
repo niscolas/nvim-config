dea22458fd3c9e408b3b63cd11866aee03c6f282
niscolas <nic.cp@hotmail.com>
2023-02-08 13:25:48 -0300

    feat: new commands

--- lua/usr/core/commands.lua
+++ lua/usr/core/commands.lua
@@ -1,45 +1,7 @@
-local prose_enabled = false
+require("usr.core.commands.prose")
 
-local enable_prose = function()
-    vim.keymap.set("n", "j", "gj")
-    vim.keymap.set("n", "k", "gk")
-    vim.keymap.set("n", "$", "g$")
-    vim.keymap.set("n", "0", "g0")
-    vim.keymap.set("n", "^", "g^")
-    vim.keymap.set("n", "I", "g^i")
-    vim.keymap.set("n", "A", "g$a")
-
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-    vim.opt.wrap = true
-
-    prose_enabled = true
-end
-
-local disable_prose = function()
-    vim.keymap.del("n", "j")
-    vim.keymap.del("n", "k")
-    vim.keymap.del("n", "$")
-    vim.keymap.del("n", "0")
-    vim.keymap.del("n", "^")
-    vim.keymap.del("n", "I")
-    vim.keymap.del("n", "A")
-
-    vim.opt.number = true
-    vim.opt.relativenumber = true
-    vim.opt.wrap = false
-
-    prose_enabled = false
-end
-
-vim.api.nvim_create_user_command(
-    "ProseToggle",
-    function(opts)
-        if prose_enabled then
-            disable_prose()
-        else
-            enable_prose()
-        end
-    end,
-    { nargs = 0 }
-)
+vim.api.nvim_create_user_command("NoFile", function(opts)
+    vim.bo.nofile = true
+    vim.bo.bufhidden = "hide"
+    vim.bo.noswapfile = true
+end, { nargs = 0 })
