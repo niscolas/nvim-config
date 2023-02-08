cf1b78774b6f2f7f980c6284afceed080846dd91
niscolas <nic.cp@hotmail.com>
2023-02-08 13:28:10 -0300

    fix: NoFile custom cmd

--- lua/usr/core/commands.lua
+++ lua/usr/core/commands.lua
@@ -3,5 +3,5 @@ require("usr.core.commands.prose")
 vim.api.nvim_create_user_command("NoFile", function(opts)
     vim.bo.buftype = "nofile"
     vim.bo.bufhidden = "hide"
-    vim.bo.noswapfile = true
+    vim.bo.swapfile = false
 end, { nargs = 0 })
