2aaf9a7c24968af2eb4b41a1c5149b6840379e08
niscolas <nic.cp@hotmail.com>
2023-04-04 17:53:22 -0300

    fix: mason ensure installed servers

--- lua/usr/modules/lsp/init.lua
+++ lua/usr/modules/lsp/init.lua
@@ -5,19 +5,19 @@ M.formatters = require("usr.modules.lsp.format")._formatters
 M.on_attach_functions =
     require("usr.modules.lsp.handlers")._post_on_attach_functions
 
-M.setup = function()
+M.get_ensure_installed_servers = function()
     local usr_lsp_core = require("usr.modules.lsp.core")
 
-    local setup_ensure_installed_servers = function()
-        for server, config in pairs(usr_lsp_core.servers) do
-            if config.ensure_installed then
-                table.insert(usr_lsp_core.ensure_installed_servers, server)
-            end
+    for server, config in pairs(usr_lsp_core.servers) do
+        if config.ensure_installed then
+            table.insert(usr_lsp_core.ensure_installed_servers, server)
         end
     end
 
-    setup_ensure_installed_servers()
+    return usr_lsp_core.ensure_installed_servers
+end
 
+M.setup = function()
     require("usr.modules.lsp.diagnostics").setup()
     require("usr.modules.lsp.handlers").setup()
     require("usr.modules.lsp.lsp_config").setup()
--- lua/usr/modules/mason.lua
+++ lua/usr/modules/mason.lua
@@ -1,12 +1,10 @@
-local usr_lsp_core = require("usr.modules.lsp.core")
+local usr_lsp = require("usr.modules.lsp")
 local mason = require("mason")
 
 mason.setup()
 
 local mason_lspconfig = require("mason-lspconfig")
 
-print("A")
 mason_lspconfig.setup {
-    ensure_installed = usr_lsp_core.ensure_installed_servers,
+    ensure_installed = usr_lsp.get_ensure_installed_servers(),
 }
-print("b")
